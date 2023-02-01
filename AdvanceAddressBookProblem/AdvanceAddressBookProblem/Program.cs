81c47304ac13c617cc9ad42ff67f5a0931a239e9
Ashwini <salunkheashwini1998@gmail.com>
2023-02-01 16:58:40 +0530

    Connection estabilished

---
+++ AdvanceAddressBookProblem/AdvanceAddressBookProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+
+namespace AdvancedAddressBookProblem
+{
+    public class Program
+    {
+        static string ConnectionStr = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PayRollService240;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(ConnectionStr);
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to Advance Addressbook Program.*****");
+
+        }
+    }
+}
